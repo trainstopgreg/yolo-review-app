7e2a6b85ed6f26c9c9ac433c4a563f82ab860a86
trainstopgreg <gregory@rightleft.com.au>
2025-06-09 13:17:40 +1000

    Create data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,9 @@
+def load_image(index):
+    # Load and return the image at the given index
+    pass
+
+def load_annotations(index):
+    # Load and return the YOLO annotations for the image at the given index
+    pass
+
+total_images = 0  # Set this to the total number of images in your dataset
